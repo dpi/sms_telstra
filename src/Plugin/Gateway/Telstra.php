c193f0be61b7872a0625a88c5705145906bc251b
dpi <pro@danielph.in>
2015-12-29 06:39:10 +0800

    Can now send SMS from plugin.
    Added configuration form and storage for key and secret.

--- src/Plugin/Gateway/Telstra.php
+++ src/Plugin/Gateway/Telstra.php
@@ -10,6 +10,7 @@ use Drupal\sms\Gateway\GatewayBase;
 use Drupal\sms\Message\SmsMessageInterface;
 use Drupal\sms\Message\SmsMessageResult;
 use Drupal\Component\Serialization\Json;
+use GuzzleHttp\Exception\RequestException;
 
 /**
  * @SmsGateway(
@@ -23,40 +24,110 @@ class Telstra extends GatewayBase {
   /**
    * {@inheritdoc}
    */
-  public function send(SmsMessageInterface $sms, array $options) {
+  public function defaultConfiguration() {
+    return [
+      'consumer_key' => '',
+      'consumer_secret' => '',
+    ];
+  }
 
-    // Configuration
-    // @todo move to module config.
-    $base_uri = 'https://api.telstra.com/v1/';
-    $client_id = '';
-    $secret = '';
-    $result = \Drupal::httpClient()->get($base_uri . 'oauth/token', [
-      'query' => [
-        'client_id' => $client_id,
-        'client_secret' => $secret,
-        'grant_type' => 'client_credentials',
-        'scope' => 'SMS'
-      ]
-    ]);
-    $response = Json::decode($result->getBody());
-    $headers = [
-      'Authorization' => 'Bearer '. $response->access_token,
-      'Content-Type' => 'application/json',
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+
+    $form['help'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => $this->t('API keys can be found at <a href="https://dev.telstra.com/user/me/apps">https://dev.telstra.com/user/me/apps</a>.'),
     ];
 
+    $form['consumer_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Consumer Key'),
+      '#default_value' => $config['consumer_key'],
+    ];
+
+    $form['consumer_secret'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Consumer Secret'),
+      '#default_value' => $config['consumer_secret'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $this->configuration['consumer_key'] = $form_state->getValue('consumer_key');
+    $this->configuration['consumer_secret'] = $form_state->getValue('consumer_secret');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function send(SmsMessageInterface $sms, array $options) {
+    $client = \Drupal::httpClient();
+    $base_uri = 'https://api.telstra.com/v1/';
+
+    $access_token = $this->telstra_authenticate();
+
     foreach ($sms->getRecipients() as $recipient) {
-      $result = \Drupal::httpClient()
-        ->post($base_uri . 'sms/messages', [
-          'headers' => $headers,
+      try {
+        $result = $client->post($base_uri . 'sms/messages', [
+          'headers' => [
+            'Authorization' => 'Bearer ' . $access_token,
+            'Content-Type' => 'application/json',
+          ],
           'json' => [
             'to' => $recipient,
             'body' => $sms->getMessage(),
-          ]
+          ],
         ]);
-      $response = Json::decode($result->getBody());
+//        $response = Json::decode($result->getBody());
+//        $response['messageId'];
+      }
+      catch (RequestException $e) {
+        // Throws 400 for invalid numbers.
+        return new SmsMessageResult(['status' => FALSE]);
+      }
+
     }
 
     return new SmsMessageResult(['status' => TRUE]);
+  }
+
+  /**
+   * Authenticate.
+   *
+   * @return string|FALSE
+   *   Access token for use in auth requests or FALSE if failed.
+   */
+  private function telstra_authenticate() {
+    $client = \Drupal::httpClient();
+
+    $base_uri = 'https://api.telstra.com/v1/';
+    $client_id = $this->configuration['consumer_key'];
+    $secret = $this->configuration['consumer_secret'];
+
+    try {
+      $result = $client->post($base_uri . 'oauth/token', [
+        'form_params' => [
+          'client_id' => $client_id,
+          'client_secret' => $secret,
+          'grant_type' => 'client_credentials',
+          'scope' => 'SMS'
+        ],
+      ]);
+      $response = Json::decode($result->getBody());
+      return $response['access_token'];
+    }
+    catch (RequestException $e) {
+      return FALSE;
+    }
   }
 
 }
